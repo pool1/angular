a1b642279bcb196a80aaeea6699853c01ad65cea
house <great1fire@gmail.com>
2016-06-29 15:06:50 +0530

    commited

--- fstApp/controller/angController.js
+++ fstApp/controller/angController.js
@@ -1,6 +1,14 @@
-var myApp =  angular.module('myApp', []);
-myApp.controller('myController','scope', ['$scope', function(){
-	
+var myApp =  angular.module('myApp', ['ngRoute']);
+myApp.config(function($routeProvider) {
 
+		$routeProvider
+
+			// route for the home page
+			.when('/main', {
+
+				templateUrl : 'view/main.html',
+			
+			});
+			 
 	
-}])
+});
